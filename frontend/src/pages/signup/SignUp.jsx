2356fad7bd811c4f2374d4430fb1c5e2179efc7d
Ankush <ankushcuber14@gmail.com>
2024-09-18 20:15:36 +0530

    some changes

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import GenderCheckBox from './GenderCheckBox';
+import GenderCheckBox from './GenderCheckbox';
 import { useState } from 'react';
 import useSignUp from '../../hooks/useSignUp';
 
