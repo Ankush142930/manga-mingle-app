7c76f0e5f98810d835f22bf477d32aa01c6a1734
Ankush <ankushcuber14@gmail.com>
2024-09-18 22:11:26 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:8000', {
+      const socket = io('https://manga-mingle-app.onrender.com', {
         query: {
           userId: authUser._id,
         },
