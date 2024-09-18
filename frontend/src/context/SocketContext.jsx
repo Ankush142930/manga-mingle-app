c688efc7ef92c00f4f7d10eeff80634254f5bfeb
Ankush <ankushcuber14@gmail.com>
2024-09-18 22:16:25 +0530

    updated the logo for the app

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useAuthContext } from './AuthContext';
